9976e3ff188aa28d85dbfc7b4d5484aea104c345
anvitha <anvitha.a@gds.ey.com>
2023-02-17 06:56:56 +0000

    message

---
+++ P2.cs
@@ -0,0 +1,62 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace EYDotNetDay2
+//{
+//    class P2
+//    {
+//        static void Main(string[] args)
+//        {
+//            FileInfo f1 = new FileInfo();
+//            Console.WriteLine(f1.FullName);
+//            Console.WriteLine(f1.Name);
+//            Console.WriteLine(f1.Length);
+//            Console.WriteLine(f1.CreationTime);
+//            Console.WriteLine(f1.LastAccessTime);
+//            Console.WriteLine(f1.LastWriteTime);
+//            Console.WriteLine(f1.Extension);
+
+//            string path = @"C:\Users\labuser\Desktop";
+            
+//            if (File.Exists(student))
+//            {
+//                Console.WriteLine("File is present");
+
+//                string[] lines = File.ReadAllLines(path);
+
+//                foreach (var l in lines)
+//                {
+//                    Console.WriteLine(l);
+//                }
+//            }
+//        }
+//    }
+//}
+
+using System;
+using System.IO; 
+
+namespace EYPrograms
+{
+    class PROJECT 
+    { 
+        static void Main(string[] args) 
+        {
+            string path = "C:\\Users\\labuser\\Desktop\\Student.txt";
+            if (File.Exists(path)) 
+            {
+                Console.WriteLine("File is present");
+                string[] lines = File.ReadAllLines(path);
+                foreach (var l in lines) 
+                {
+                    Console.WriteLine(l);
+                }
+            } 
+            else Console.WriteLine("File does not exist");
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace EYDotNetDay2
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			char[] arr = { 'a', 'e', 'i', 'o', 'u' };
+//			String s1 = new String(arr);
+//			Console.WriteLine(s1);
+//			Console.ReadLine();
+//			//----------------------------------------
+
+//			string s2 = "Welcome to C#";
+//			Console.WriteLine(s2);
+//			Console.ReadLine();
+
+//			//----------------------------------------
+
+//			string s3 = "Welcome to EY GDS";
+//			Console.WriteLine(s3);
+//			Console.ReadLine();
+
+//			//----------------------------------------
+
+//			StringBuilder addWord = new StringBuilder("Word1");
+//			addWord.Append("word2");
+//			addWord.Append("word3");
+//			addWord.Append(" It's Friday today!");
+//			addWord.Append("Have an amazing weekend!");
+//			addWord.Append("Monday is near...");
+//			addWord.Remove(5, 10); //word2 is removed
+//			Console.WriteLine(addWord);
+//			Console.ReadLine();
+
+
+//		}
+//	}
+//}
+
+
+//using System;
+
+//namespace A
+//{
+//    class myClass1
+//    {
+//        public void function1()
+//        {
+//            Console.WriteLine("A namespace-myClass1--function1()");
+//        }
+//    }
+//}
+
+//namespace B
+//{
+//    class myClass2
+//    {
+//        public void function2()
+//        {
+//            Console.WriteLine("B namespace-myClass2--function2()");
+//            Console.ReadLine();
+//        }
+//    }
+//}
+
+//class india
+//{
+//    static void Main(string[] args)
+//    {
+//        A.myClass1 obj1 = new A.myClass1();
+//        obj1.function1();
+
+//        B.myClass2 obj2 = new B.myClass2();
+//        obj2.function2();
+//    }
+//}
+
+
+
+
+
+
+//   string path = @"c:\demo\dummyfile.txt";
+
+//if (File.Exists(path))
+//{
+//	
+//}
+
