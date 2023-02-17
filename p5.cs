c3730e0d804b40eae70f7bbdfc024ca747538c4f
anvitha <anvitha.a@gds.ey.com>
2023-02-17 10:20:02 +0000

    message

---
+++ p5.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EYDotNetDay2
+{
+    class p5
+    {
+        static void Main(string[] args)
+        {
+            demo1 o1 = new demo1();
+            demo2 o2 = new demo2();
+            demo3 o3 = new demo3();
+
+            o1.func1();
+            o2.func2();
+            o3.func3();
+
+            Console.ReadLine();
+        }
+    }
+    class demo1
+    {
+        public void func1()
+        {
+            Console.WriteLine("demo1--func1() is invoked");
+        }
+    }
+    class demo2
+    {
+        public void func2()
+        {
+            Console.WriteLine("demo2--func2() is invoked");
+        }
+    }
+    class demo3
+    {
+        public void func3()
+        {
+            Console.WriteLine("demo3--func3() is invoked");
+        }
+    }
+}
